3cd3d3a3502dcd75d934d1c5f23790d635ee091f
Nick <nick.buscemi13@gmail.com>
2023-07-28 14:32:23 -0400

    finished

---
+++ server/db/models/color.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Color extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Color.init({
+    name: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+      unique: true,
+      validate:{
+        notEmpty: {
+          msg: "Name can't be empty"
+        }
+      }
+    }
+    
+  }, {
+    sequelize,
+    modelName: 'Color',
+  });
+  return Color;
+};
